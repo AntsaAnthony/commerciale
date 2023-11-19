99cfe57558f5f7432e3361f2471c0ecd1542f9f4
TolotraArnaud <ard.tolotra@gmail.com>
2023-11-19 12:01:13 +0300

    module login and adding dependance for laravel projects

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', [\App\Http\Controllers\LoginController::class, 'login'])->name('auth.login');
+Route::post('/login', [\App\Http\Controllers\LoginController::class, 'doLogin']);
+
+Route::prefix('/comm')->name('comm.')->controller(\App\Http\Controllers\CommController::class)->group(function (){
+    Route::get('/', 'index')->name('index');
+});
+
+Route::prefix('/provider')->name('fournisseur.')->controller(\App\Http\Controllers\CommController::class)->group(function (){
+    Route::get('/', 'index')->name('index');
 });
